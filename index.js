a9d554eff398ad1767550ca345f9422d7c1a83f4
WinnerNew <775405088@qq.com>
2019-08-19 19:21:20 +0800

    up

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var fn = _.template(html);
 html = fn({name: 'AA'})
 console.log(html);
 console.log(fn.toString())
+var str = 'abcd';
+console.log(str.split('').reverse().join(''));
+var st2 = [...str].reverse().join('');
+console.log(st2);
